499e3250f3c97cd98c08329e6779e390cc0cce6c
fredatwox <freddiecom10@gmail.com>
2025-05-05 13:02:26 +0000

    schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 
 
@@ -6,9 +7,16 @@ const userSchema = new Schema({
   fullname: {type: String, required: true, unique: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  confirmPassword:  { type: String, required: true },
   role: { type: String, enum: ["User", "Employer", "Admin", "superAdmin"], default: "User" },
 
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 
