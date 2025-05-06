69ba38624404a3d049b1859b9793fd57d1736dc2
fredatwox <freddiecom10@gmail.com>
2025-05-06 15:51:03 +0000

    consult

--- models/consultant.js
+++ models/consultant.js
@@ -7,7 +7,7 @@ const consultantSchema = new Schema(
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     professionalTitle:{ type: String, required: true},    
-    yearsOfExperience:{ type: String, required: true},    
+    yearsOfExperience:{ type: Number, required: true},    
     industry: { type: String, required: true }, // industry or specialization
   },
   { timestamps: true }
